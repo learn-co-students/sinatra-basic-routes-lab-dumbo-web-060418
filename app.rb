02f87c6c59684bb8271255e6fc7e82f22c7114e0
TezSmith <Montez.Smith1@gmail.com>
2018-06-24 12:12:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+     "My name is Tez."
+  end
+
+  get '/hometown' do
+     "My hometown is Philly."
+  end
+
+  get '/favorite-song' do
+     "My favorite song is One in a Million by Aaliyah!"
+  end
+
 end
