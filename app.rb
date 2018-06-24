d1ef9a742bd44bc043fa27db50637dc718f02059
mark sauer-utley <mark.utley.713@gmail.com>
2018-06-24 13:46:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Mark"
+  end
+
+  get '/hometown' do
+    "My hometown is Fredericksburg, TX"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Pictures of You by The Cure"
+  end
+
 end
